a89173bd5f60dc19bc6a9a5fc4919a14f120c855
Jared Nolen <Legodoghead@gmail.com>
2021-10-22 10:44:44 -0700

    Star now only bounces off bounce tiles. has no max bounces

--- Quest for the Ludwig Star/Assets/Scripts/StarControl.cs
+++ Quest for the Ludwig Star/Assets/Scripts/StarControl.cs
@@ -11,7 +11,6 @@ public class StarControl : MonoBehaviour
     // Public variables
     public Vector2 direction;
     public float speed = 10;
-    public float bounces = 1;
     public float life = 2.5f;
 
     // Start is called before the first frame update
@@ -34,22 +33,20 @@ public class StarControl : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D col)
     {
         //Debug.Log("Hit " + col.gameObject.tag);
-        if (col.gameObject.tag == "Ground")
+        if (col.gameObject.tag == "Bounce")
         {
             Vector3 reflect = Vector3.Reflect(transform.right, col.contacts[0].normal);
             float rot = 90 - Mathf.Atan2(reflect.z, reflect.x) * Mathf.Rad2Deg;
             transform.eulerAngles = new Vector3(0, 0, rot);
-
-            if (bounces > 0) bounces -= 1;
-            else if (bounces <= 0)
-            {
-                pc.starOut = false;
-                Destroy(gameObject);
-            }
         }
 
-        //Debug.Log("Hit " + col.gameObject.tag);
         if (col.gameObject.tag == "Hazard")
+        {
+            pc.starOut = false;
+            Destroy(gameObject);
+        }
+
+        if (col.gameObject.tag == "Ground")
         {
             pc.starOut = false;
             Destroy(gameObject);
